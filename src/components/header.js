50b755c5e164b5b4745dc0444e0887bc25d711e0
Oğuz EROL <oguzerol91@gmail.com>
2018-01-18 17:43:50 +0300

    reactstrap update

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,40 @@
 import React from 'react';
-import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Nav, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
   render() {
     return (
       <div>
         <Nav>
           <NavItem>
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+              <DropdownToggle caret>
+                Dropdown
+        </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem header>Header</DropdownItem>
+                <DropdownItem disabled>Action</DropdownItem>
+                <DropdownItem>Another Action</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Another Action</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
             <NavLink tag={Link} to="/">Home</NavLink>
           </NavItem>
           <NavItem>
@@ -18,3 +45,4 @@ export default class Header extends React.Component {
     );
   }
 }
+
