8efbd791cd7e382a4a94bc9df6114aefc62735b8
Oğuz EROL <oguzerol91@gmail.com>
2018-01-23 17:45:55 +0300

    banner & slider added

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -6,7 +6,7 @@ export default class Jumbotron extends React.Component {
     render() {
         return (
             <div className={Styles.wrapper}>
-                <video src={require('../../assets/video/jumbotron.mp4')} autoPlay muted loop className={Styles.video_wrapper} />
+                <video src={require('../../assets/video/videoplayback.webm')} autoPlay muted loop className={Styles.video_wrapper} />
 
                 <div className={Styles.slider_wrapper}>
                     <Slider />
--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -3,29 +3,28 @@ import Swiper from 'react-id-swiper';
 import Styles from './slider.scss';
 
 
+
 export default class Slider extends React.Component {
     render() {
         const params = {
-            pagination: {
-                el: '.swiper-pagination',
-                type: 'bullets',
-                clickable: true
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev'
-            },
-            spaceBetween: 30
+            slidesPerView: 3,
+            freeMode: true,
+            loop: true,
         }
 
         return (
             <div  className={Styles.wrapper}>
                 <Swiper {...params}>
-                    <div>Slide 1</div>
-                    <div>Slide 2</div>
-                    <div>Slide 3</div>
-                    <div>Slide 4</div>
-                    <div>Slide 5</div>
+                    <div className={Styles.item}>Slide 1</div>
+                    <div className={Styles.item}>Slide 2</div>
+                    <div className={Styles.item}>Slide 3</div>
+                    <div className={Styles.item}>Slide 4</div>
+                    <div className={Styles.item}>Slide 5</div>
+                    <div className={Styles.item}>Slide 6</div>
+                    <div className={Styles.item}>Slide 7</div>
+                    <div className={Styles.item}>Slide 8</div>
+                    <div className={Styles.item}>Slide 9</div>
+                    <div className={Styles.item}>Slide 10</div>
                 </Swiper>
             </div>
         )
