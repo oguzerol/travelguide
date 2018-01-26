b4031e549811542478a51a20b736741c662d8cc8
Oğuz EROL <oguzerol91@gmail.com>
2018-01-26 17:36:01 +0300

    slider item development

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -15,7 +15,14 @@ export default class Slider extends React.Component {
         return (
             <div  className={Styles.wrapper}>
                 <Swiper {...params}>
-                    <div className={Styles.item}>Slide 1</div>
+                    <div className={Styles.item}>
+                        <div className={Styles.content} > 
+                          <img src="https://i.redditmedia.com/zzbgy_qMQlyNziyiK2l7tlZzFUb6j1vwEuQGJITK2BI.jpg?s=640925f6fc13eebef1df23ccbebf76d1" alt="text"/>
+                          <div className={Styles.text_wrapper}>
+                                <p> test </p>
+                           </div> 
+                        </div>
+                    </div>
                     <div className={Styles.item}>Slide 2</div>
                     <div className={Styles.item}>Slide 3</div>
                     <div className={Styles.item}>Slide 4</div>
